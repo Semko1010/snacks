0e9fb68eb0d523395ea81fe6ab5382857b037bcb
semir-hamidovic <semir.hamidovic@shetani.de>
2023-09-20 12:59:49 +0200

    v1.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,129 @@
-"use client"
-import { useEffect, useState } from "react"
-import Header from "../../components/header/Header"
-import Products from "../../public/products.json"
-interface product{
-      name: string,
-      Inhalt:string,
-      Inhaltsstoffe:string,
-      category:string,
-      image:string
+"use client";
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import ButtonRecept from "../../components/button/Button";
+import Products from "../../public/products.json";
+interface product {
+	name: string;
+	Inhalt: string;
+	Inhaltsstoffe: string;
+	category: string;
+	image: string;
 }
 
-interface data{
-    products:product
-    setProducts:React.Dispatch<React.SetStateAction<product>>
+interface data {
+	products: product;
+	setProducts: React.Dispatch<React.SetStateAction<product>>;
 }
 
 export default function Home() {
-  const [products,setProducts] = useState<any>(Products)
-  const [search,setSearch] = useState("")
-  const [changeValue,setChangeValue] = useState(true)
-  const [showFaq, setShowFaq] = useState<[] | null>()
+	const [products, setProducts] = useState<any>(Products);
+	const [search, setSearch] = useState("");
+	const [changeValue, setChangeValue] = useState(true);
+	const [showFaq, setShowFaq] = useState<[] | null>();
 
-  const toggle = (item: []) => {
-    if (showFaq == item) {
-      return setShowFaq(null)
-    }
-    setShowFaq(item)
-  }
- 
+	const toggle = (item: []) => {
+		if (showFaq == item) {
+			return setShowFaq(null);
+		}
+		setShowFaq(item);
+	};
 
+	return (
+		<main className='bg-white-900'>
+			<Header
+				search={{ search, setSearch }}
+				changeValue={{ changeValue, setChangeValue }}
+			/>
 
-
-  return (
-    <main className="bg-white-900">
-     <Header search={{search,setSearch}} changeValue={{changeValue,setChangeValue}}/>
-     
-     {changeValue ? products?.filter((i: { name: string }) => {
-     
-      
+			{changeValue
+				? products
+						?.filter((i: { name: string }) => {
 							return search.toLowerCase() === ""
 								? i
 								: i.name.toLowerCase().includes(search);
-						}).map((item:any,index:any)=>{
-              return(
-                <div key={index} className="flex flex-col justify-center items-center p-6 border m-6 bg-[white]">
-                
-                  <h2>{item.name}</h2>
-                  <img className="w-40" src={`${item.image}`} alt="" />
-                  <button onClick={() => toggle(item)} className="animate-bounce border py-2 px-4 m-2">Mehr anzeigen</button>
-                  {showFaq == item ? <div>
-                  <p className="text-center">{item.Inhaltsstoffe}</p>
-                  </div> : null }
-                  
-                  
-                </div>
-              )
-            }) : products?.filter((i: {
-              category: any
-}) => {
-     
-      
+						})
+						.map((item: any, index: any) => {
+							return (
+								<div
+									key={index}
+									className='py-2 flex flex-col justify-center items-center  border m-6 bg-[white]'>
+									<h2 className='text-xl text-[#434342]'>{item.name}</h2>
+									<img className='w-42' src={`${item.image}`} alt='' />
+									<button
+										onClick={() => toggle(item)}
+										className='  py-2 px-4 m-2'>
+										<ButtonRecept />
+									</button>
+									<div
+										className='p-2'
+										style={
+											showFaq == item
+												? {
+														opacity: 1,
+														height: "auto",
+														maxHeight: "9999px",
+														transition: " all 0.5s cubic-bezier(1, 0, 1, 0)",
+												  }
+												: {
+														opacity: 0,
+														maxHeight: 0,
+														overflow: "hidden",
+														transition: " all 0.5s cubic-bezier(0, 1, 0, 1)",
+												  }
+										}>
+										<p className='text-center text-[#434342]'>
+											{item.Inhaltsstoffe}
+										</p>
+									</div>
+								</div>
+							);
+						})
+				: products
+						?.filter((i: { category: any }) => {
 							return search.toLowerCase() === ""
 								? i
 								: i.category.toLowerCase().includes(search);
-						}).map((item:any,index:any)=>{
-              return(
-                <div key={index} className="flex flex-col justify-center items-center p-6 border m-6">
-                
-                  <h2>{item.name}</h2>
-                  <img className="w-42" src={`${item.image}`} alt="" />
-                  <button onClick={() => toggle(item)} className="border py-2 px-4 m-2 animate-bounce">Mehr anzeigen</button>
-                  
-                  {showFaq == item ? <div>
-                  <p className="text-center">{item.Inhaltsstoffe}</p>
-                  </div> : null }
-                  
-                  
-                </div>
-              )
-            })}
-{/* {Products.map((item,index) =>{
+						})
+						.map((item: any, index: any) => {
+							return (
+								<div
+									key={index}
+									className='py-2 flex flex-col justify-center items-center  border m-6'>
+									<h2 className='text-[#434342]'>{item.name}</h2>
+									<img className='w-42' src={`${item.image}`} alt='' />
+
+									<button
+										onClick={() => toggle(item)}
+										className=' py-2 px-4 m-2'>
+										<ButtonRecept />
+									</button>
+
+									<div
+										className=' w-full p-2'
+										style={
+											showFaq == item
+												? {
+														opacity: 1,
+														height: "auto",
+														maxHeight: "9999px",
+														transition: " all 0.5s cubic-bezier(1, 0, 1, 0)",
+												  }
+												: {
+														opacity: 0,
+														maxHeight: 0,
+														overflow: "hidden",
+														transition: " all 0.5s cubic-bezier(0, 1, 0, 1)",
+												  }
+										}>
+										<p className='text-center text-[#434342]'>
+											{item.Inhaltsstoffe}
+										</p>
+									</div>
+								</div>
+							);
+						})}
+			{/* {Products.map((item,index) =>{
   
   return(
     <div key={index} className="flex flex-col justify-center items-center p-6 ">
@@ -91,6 +135,6 @@ export default function Home() {
   )
   
 })} */}
-    </main>
-  )
+		</main>
+	);
 }
